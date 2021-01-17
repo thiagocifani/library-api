31451ec66ba2cd66f297266c9f72f9ffd6e8e137
thiagocifani <cifani.thiago@gmail.com>
2021-01-17 17:19:40 -0300

    Add filter to author resource.

--- app/resources/author_resource.rb
+++ app/resources/author_resource.rb
@@ -1,3 +1,14 @@
 class AuthorResource < JSONAPI::Resource
   attributes :first, :last
+  filters :query
+
+  def self.apply_filter(records, filter, value, options)
+    case filter
+      when :query
+        records.where('last LIKE ?', "%#{value.first}%")
+          .or(records.where('first LIKE ?', "%#{value.first}%"))
+      else
+        super
+    end
+  end
 end
