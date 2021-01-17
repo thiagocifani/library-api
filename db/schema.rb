3be40732c2c63fbb4d9806ed0b90de2c6db8cf3d
thiagocifani <cifani.thiago@gmail.com>
2021-01-17 20:10:23 -0300

    Add book rails resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_28_184800) do
+ActiveRecord::Schema.define(version: 2021_01_17_225742) do
 
   create_table "authors", force: :cascade do |t|
     t.string "first"
@@ -19,4 +19,15 @@ ActiveRecord::Schema.define(version: 2020_12_28_184800) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "isbn"
+    t.date "publish_date"
+    t.integer "author_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_books_on_author_id"
+  end
+
+  add_foreign_key "books", "authors"
 end
